71fb0b69a1d247bd3d5a6dcf2f756b12b9220986
John Lambert <johnwl@stanford.edu>
2020-05-30 15:55:43 -0400

    Add files via upload

--- waymo_raw_data_to_argoverse.py
+++ waymo_raw_data_to_argoverse.py
@@ -28,6 +28,14 @@ https://arxiv.org/pdf/1912.04838.pdf
 
 from scipy.spatial.transform import Rotation
 
+CAMERA_NAMES = [
+    'unknown', # 0, 'UNKNOWN',
+    'ring_front_center', # 1, 'FRONT'
+    'ring_front_left', # 2, 'FRONT_LEFT',
+    'ring_front_right', # 3, 'FRONT_RIGHT',
+    'ring_side_left', # 4, 'SIDE_LEFT',
+    'ring_side_right', # 5, 'SIDE_RIGHT'
+]
 
 def check_mkdir(dirpath):
 	""" """
@@ -250,33 +258,46 @@ def main():
 		'4854173791890687260_2880_000_2900_000',
 		'272435602399417322_2884_130_2904_130'
 	]
-	CAMERA_NAMES = [
-	    'unknown', # 0, 'UNKNOWN',
-	    'ring_front_center', # 1, 'FRONT'
-	    'ring_front_left', # 2, 'FRONT_LEFT',
-	    'ring_front_right', # 3, 'FRONT_RIGHT',
-	    'ring_side_left', # 4, 'SIDE_LEFT',
-	    'ring_side_right', # 5, 'SIDE_RIGHT'
-	]
+
+	save_images = False
+	save_poses = False
+
 	img_count = 0
 	for log_id in log_ids:
 		print(log_id)
 		tfrecord_name = f'segment-{log_id}_with_camera_labels.tfrecord'
 		tf_fpath = f'{TFRECORD_DIR}/{tfrecord_name}'
 		dataset = tf.data.TFRecordDataset(tf_fpath, compression_type='')
+		
+		log_calib_json = None
+
 		for data in dataset:
 			frame = open_dataset.Frame()
 			frame.ParseFromString(bytearray(data.numpy()))
 			# discovered_log_id = '967082162553397800_5102_900_5122_900'
 			assert log_id == frame.context.name
 			
-			SE3_flattened = np.array(frame.pose.transform)
-			city_SE3_egovehicle = SE3_flattened.reshape(4,4)
 			# Frame start time, which is the timestamp of the first top lidar spin
 			# within this frame, in microseconds
 			timestamp_ms = frame.timestamp_micros
 			timestamp_ns = timestamp_ms * 1000 # to nanoseconds
-			dump_pose(city_SE3_egovehicle, timestamp_ns, log_id)
+			SE3_flattened = np.array(frame.pose.transform)
+			city_SE3_egovehicle = SE3_flattened.reshape(4,4)
+			if save_poses:
+				dump_pose(city_SE3_egovehicle, timestamp_ns, log_id)
+
+			
+			calib_json = form_calibration_json(frame.context.camera_calibrations)
+			if log_calib_json is None:
+				log_calib_json = calib_json
+
+				calib_json_fpath = f'pose_logs/{log_id}/vehicle_calibration_info.json'
+				check_mkdir(str(Path(calib_json_fpath).parent))
+				save_json_dict(calib_json_fpath, calib_json)
+
+			else:
+				# pdb.set_trace()
+				assert calib_json == log_calib_json	
 
 			# 5 images per frame
 			for index, tf_cam_image in enumerate(frame.images):
@@ -292,9 +313,9 @@ def main():
 				# tf_cam_image.shutter
 				# tf_cam_image.camera_trigger_time
 				# tf_cam_image.camera_readout_done_time
-				dump_pose(city_SE3_egovehicle, timestamp_ns, log_id)
+				if save_poses:
+					dump_pose(city_SE3_egovehicle, timestamp_ns, log_id)
 
-				save_images = False
 				if save_images:
 					camera_name = CAMERA_NAMES[tf_cam_image.name]
 					img = tf.image.decode_jpeg(tf_cam_image.image)
@@ -310,6 +331,50 @@ def main():
 				# pose_save_fpath = f'logs/{log_id}/poses/city_SE3_egovehicle_{timestamp_ns}.json'
 				# assert not Path(pose_save_fpath).exists()
 				# save_json_dict(pose_save_fpath)
+
+
+RING_IMAGE_SIZES = {
+	# width x height
+	'ring_front_center': (1920, 1280),
+	'ring_front_left':  (1920, 1280),
+	'ring_side_left': (1920, 886),
+	'ring_side_right': (1920,886)
+}
+
+def form_calibration_json(calib_data):
+	""" """
+	calib_dict = {
+		'camera_data_': []
+	}
+	for camera_calib in calib_data:
+
+		cam_name = CAMERA_NAMES[camera_calib.name]
+		city_SE3_egovehicle = np.array(camera_calib.extrinsic.transform).reshape(4,4)
+		x,y,z = city_SE3_egovehicle[:3,3]
+		R = city_SE3_egovehicle[:3,:3]
+		assert np.allclose( city_SE3_egovehicle[3], np.array([0,0,0,1]) )
+		q = rotmat2quat(R)
+		qw, qx, qy, qz = q
+		f_u, f_v, c_u, c_v, k1, k2, p1, p2, k3 = camera_calib.intrinsic
+
+		cam_dict = {
+			'key': 'image_raw_' + cam_name,
+			'value': {
+				'focal_length_x_px_': f_u,
+				'focal_length_y_px_': f_v,
+				'focal_center_x_px_': c_u,
+				'focal_center_y_px_': c_v,
+				'skew_': 0,
+				'distortion_coefficients_': [0,0,0],
+				'vehicle_SE3_camera_': {
+					'rotation': {'coefficients': [qw, qx, qy, qz] },
+					'translation': [x,y,z]
+				}
+			}
+		}
+		calib_dict['camera_data_'] += [cam_dict]
+
+	return calib_dict
 
 
 def dump_pose(city_SE3_egovehicle, timestamp, log_id):
