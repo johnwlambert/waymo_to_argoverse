a8a443697c4f0b570cacd03ca9dc194d6d470eb6
John Lambert <johnlambert@gatech.edu>
2021-09-23 15:08:51 -0600

    reformat python black

--- waymo2argo/launch_all_trackers.py
+++ waymo2argo/launch_all_trackers.py
@@ -6,28 +6,28 @@ from mseg_semantic.utils.subprocess_utils import run_command
 
 
 def launch_all_trackers():
-	""" """
-	split = 'test' # 'val'
-	dets_dataroot = '/w_o_d/detections'
+    """ """
+    split = "test"  # 'val'
+    dets_dataroot = "/w_o_d/detections"
 
-	min_conf = 0.475
-	min_hits = 6
-		
-	tracks_dump_dir = f'/w_o_d/ab3dmot_tracks_conf{min_conf}_complete_sharded_{split}_minhits{min_hits}'
-	
-	if split == 'val':
-		log_ids = get_val_log_ids()
-	elif split == 'test':
-		log_ids = get_test_log_ids()
-	
-	for log_id in log_ids:
-		pose_dir = f'/w_o_d/{split.upper()}_RAW_DATA_SHARDED/sharded_pose_logs/{split}_{log_id}'
+    min_conf = 0.475
+    min_hits = 6
 
-		cmd = f'sbatch -p cpu -c 5'
-		cmd += f' run_tracker.sh {split} {dets_dataroot} {pose_dir} {tracks_dump_dir} {min_conf} {min_hits}'
-		print(cmd)
-		run_command(cmd)
+    tracks_dump_dir = f"/w_o_d/ab3dmot_tracks_conf{min_conf}_complete_sharded_{split}_minhits{min_hits}"
+
+    if split == "val":
+        log_ids = get_val_log_ids()
+    elif split == "test":
+        log_ids = get_test_log_ids()
+
+    for log_id in log_ids:
+        pose_dir = f"/w_o_d/{split.upper()}_RAW_DATA_SHARDED/sharded_pose_logs/{split}_{log_id}"
+
+        cmd = f"sbatch -p cpu -c 5"
+        cmd += f" run_tracker.sh {split} {dets_dataroot} {pose_dir} {tracks_dump_dir} {min_conf} {min_hits}"
+        print(cmd)
+        run_command(cmd)
 
 
-if __name__ == '__main__':
-	launch_all_trackers()
+if __name__ == "__main__":
+    launch_all_trackers()
