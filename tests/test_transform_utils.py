ea9728e3fecb62259294827e7ad8115059678360
John Lambert <johnlambert@gatech.edu>
2022-01-07 07:28:49 -0700

    add module as namespace

--- tests/test_transform_utils.py
+++ tests/test_transform_utils.py
@@ -10,8 +10,8 @@ def test_transform() -> None:
     yaw = 90
     for yaw in np.random.randn(10) * 360:
         R = rotMatZ_3D(yaw)
-        w, x, y, z = rotmat2quat(R)
-        qx, qy, qz, qw = yaw_to_quaternion3d(yaw)
+        w, x, y, z = transform_utils.rotmat2quat(R)
+        qx, qy, qz, qw = transform_utils.yaw_to_quaternion3d(yaw)
 
         print(w, qw)
         print(x, qx)
@@ -26,8 +26,8 @@ def test_transform() -> None:
 def test_cycle() -> None:
     """ """
     R = np.eye(3)
-    q = rotmat2quat(R)
-    R_cycle = quat2rotmat(q)
+    q = transform_utils.rotmat2quat(R)
+    R_cycle = transform_utils.quat2rotmat(q)
     assert np.allclose(R, R_cycle)
 
 
@@ -35,9 +35,9 @@ def test_quaternion3d_to_yaw() -> None:
     """ """
     num_trials = 100000
     for yaw in np.linspace(-np.pi, np.pi, num_trials):
-        qx, qy, qz, qw = yaw_to_quaternion3d(yaw)
+        qx, qy, qz, qw = transform_utils.yaw_to_quaternion3d(yaw)
         q_argo = np.array([qw, qx, qy, qz])
-        new_yaw = quaternion3d_to_yaw(q_argo)
+        new_yaw = transform_utils.quaternion3d_to_yaw(q_argo)
         assert np.isclose(yaw, new_yaw)
         if not np.allclose(yaw, new_yaw):
             print(yaw, new_yaw)
